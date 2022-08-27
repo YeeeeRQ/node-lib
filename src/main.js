3258447ebacc52b264253a41a8c269402393149a
Wox <YeeeeRQ@outlook.com>
2022-08-27 09:52:27 +0800

    添加两个路由

--- src/main.js
+++ src/main.js
@@ -1,12 +1,22 @@
 const Koa = require("koa");
+const Router = require("koa-router");
 
 const { APP_PORT } = require("./config/config.default");
 
 const app = new Koa();
 
-app.use((ctx, next) => {
-  ctx.body = "hello world";
+const indexRouter = new Router();
+indexRouter.get("/", (ctx, next) => {
+  ctx.body = "hello index";
 });
+
+const userRouter = new Router();
+userRouter.get("/users", (ctx, next) => {
+  ctx.body = "hello user router";
+});
+
+app.use(indexRouter.routes());
+app.use(userRouter.routes());
 
 app.listen(APP_PORT, () => {
   console.log(`server is running on http://localhost:${APP_PORT}`);
